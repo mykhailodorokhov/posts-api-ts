1fd689bc23d48bbda525cafa859f0feec3299e0c
Mykhailo Dorokhov <mykhailo.dorokhov@pipedrive.com>
2023-10-10 22:38:21 +0300

    type names are fixed

--- src/routes/post-posts.ts
+++ src/routes/post-posts.ts
@@ -1,16 +1,16 @@
 import { FastifyInstance, RouteOptions } from "fastify";
 import * as postsController from "../controllers/posts.controller";
-import { postBodySchema, postBodyType } from "./schemas/posts.schema";
+import { PostBodySchema, PostBodyType } from "./schemas/posts.schema";
 
 export default function createPost(fastify: FastifyInstance): RouteOptions {
   return {
     method: "POST",
     url: "/api/posts",
     schema: {
-      body: postBodySchema,
+      body: PostBodySchema,
     },
     handler: async (request, reply) => {
-      const newPost = request.body as postBodyType;
+      const newPost = request.body as PostBodyType;
       const posts = await postsController.createPost(newPost);
       reply.code(201).send(posts);
     },
